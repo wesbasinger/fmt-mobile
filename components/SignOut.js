80cb0d0d41335b3300a46a203174355c22059262
Wes Basinger <wbasinger@villagetechschools.org>
2018-01-15 19:00:24 -0600

    Stubbed out the sign out selector.

--- components/SignOut.js
+++ components/SignOut.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, Picker } from 'react-native';
 
 export default class SignOut extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {hourId: ""}
+  }
   render() {
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>
-          This is a signout
-        </Text>
+      <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center'}}>
+        <Text>Worker</Text>
+        <Picker
+          selectedValue={this.state.hourId}
+          onValueChange={(val) => {this.setState({hourId: val})}}>
+          <Picker.Item value="" label="---"/>
+          <Picker.Item value="Wes Basinger" label="Evil Twin"/>
+          <Picker.Item value="Wesley Basinger" label="The Real Wes"/>
+        </Picker>
+        <Text>{this.state.hourId}</Text>
       </View>
     );
   }
